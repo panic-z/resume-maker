af96c4903c690c1d0cdc2b05b8141cff55f0753c
Daniel <daniel@lightleap.cn>
2026-04-07 00:05:09 +0800

    feat: add Preview component and classic template CSS

---
+++ src/components/Preview.tsx
@@ -0,0 +1,18 @@
+import "../templates/classic.css";
+import type { TemplateName } from "../lib/storage";
+
+interface PreviewProps {
+  html: string;
+  template: TemplateName;
+}
+
+export function Preview({ html, template }: PreviewProps) {
+  return (
+    <div className="h-full overflow-auto bg-gray-100 p-8 flex justify-center">
+      <div
+        className={`template-${template} bg-white shadow-lg w-[210mm] min-h-[297mm] p-[20mm] text-sm leading-relaxed`}
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </div>
+  );
+}
