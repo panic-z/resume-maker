8447447f6e12e7ee27cfdd5bc31f060093ba832e
Daniel <daniel@lightleap.cn>
2026-04-07 00:11:58 +0800

    feat: wire up App with Header, ResumePage, and complete integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Header } from "./components/Header";
+import { ResumePage } from "./components/ResumePage";
+
 function App() {
-  return <div className="h-screen">Resume Maker</div>;
+  return (
+    <div className="h-screen flex flex-col bg-gray-50">
+      <Header />
+      <main className="flex-1 overflow-hidden">
+        <ResumePage />
+      </main>
+    </div>
+  );
 }
 
 export default App;
